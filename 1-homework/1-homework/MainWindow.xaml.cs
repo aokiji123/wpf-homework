13a6b95058a8422cdf7122244d075e0b041ff843
Bohdan Tolmachov <bohdan.tolmachov@atreo.cz>
2023-12-09 13:04:00 +0100

    homework

---
+++ 1-homework/1-homework/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace _1_homework
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int clickCount = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            clickCount++;
+            MessageBox.Show($"Clicks count: {clickCount}");
+        }
+    }
+}
