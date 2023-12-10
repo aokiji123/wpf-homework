26c6da1e43c4e47c4c10f57b92726c716af333fa
Bohdan Tolmachov <bohdan.tolmachov@atreo.cz>
2023-12-11 00:23:31 +0100

    homework

---
+++ 2-homework/2-homework/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace _2_homework
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ 3-homework/3-homework/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Effects;
+using System.Windows.Media.Imaging;
+
+namespace _3_homework
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Uri imageUri = new Uri("https://www.wfla.com/wp-content/uploads/sites/71/2023/05/GettyImages-1389862392.jpg?w=2560&h=1440&crop=1", UriKind.RelativeOrAbsolute);
+            BitmapImage bitmapImage = new BitmapImage(imageUri);
+            imageView.Source = bitmapImage;
+
+            blurSlider.ValueChanged += BlurSlider_ValueChanged;
+        }
+
+        private void BlurSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            double blurRadius = blurSlider.Value;
+            ApplyBlurEffect(blurRadius);
+        }
+
+        private void ApplyBlurEffect(double blurRadius)
+        {
+            BlurEffect blurEffect = new BlurEffect();
+            blurEffect.Radius = blurRadius;
+            imageView.Effect = blurEffect;
+        }
+    }
+}
