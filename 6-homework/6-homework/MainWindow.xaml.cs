c78d739d222b4f392562f526920302c5e63d7bf9
Bohdan Tolmachov <bohdan.tolmachov@atreo.cz>
2023-12-13 00:26:07 +0100

    homework

---
+++ 6-homework/6-homework/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace _6_homework
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Key CTRL_R = Key.R;
+        private Key CTRL_G = Key.G;
+        private Key CTRL_B = Key.B;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == CTRL_R && Keyboard.Modifiers == ModifierKeys.Control)
+            {
+                ColorButton.Background = Brushes.Red;
+            }
+            else if (e.Key == CTRL_G && Keyboard.Modifiers == ModifierKeys.Control)
+            {
+                ColorButton.Background = Brushes.Green;
+            }
+            else if (e.Key == CTRL_B && Keyboard.Modifiers == ModifierKeys.Control)
+            {
+                ColorButton.Background = Brushes.Blue;
+            }
+        }
+    }
+}
---
+++ 7-homework/7-homework/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace _7_homework
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public class Student
+        {
+            public int Number { get; set; }
+            public string Name { get; set; }
+            public bool IsPresent { get; set; }
+            public bool IsLate { get; set; }
+            public bool IsAbsent { get; set; }
+            public string ExamGrade { get; set; }
+            public string LessonGrade { get; set; }
+        }
+
+        public ObservableCollection<Student> Students { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Students = new ObservableCollection<Student>()
+            {
+                new Student { Number = 1, Name = "John Doe Doedovich", IsPresent = true, ExamGrade = "-", LessonGrade = "12" },
+                new Student { Number = 2, Name = "Anna Smith Smithovna", IsPresent = true, ExamGrade = "-", LessonGrade = "11" },
+                new Student { Number = 3, Name = "Mark Cukerberg Cukerbergovich", IsLate = true, ExamGrade = "12", LessonGrade = "11" },
+                new Student { Number = 4, Name = "Bohdan Tolmachov Evgenievich", IsPresent = true, ExamGrade = "-", LessonGrade = "12" },
+            };
+
+            DataContext = this;
+        }
+    }
+}
